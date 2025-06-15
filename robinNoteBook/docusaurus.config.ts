9bfe03191181086fd354186055dfa569f173c3ef
EagerGrb <282113895@qq.com>
2025-06-15 16:32:09 +0800

    增加自动部署代码

--- robinNoteBook/docusaurus.config.ts
+++ robinNoteBook/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://eagergrb.github.io/',
+  url: 'https://eagergrb.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/robinNoteBook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'EagerGrb', // Usually your GitHub org/user name.
+  projectName: 'robinNoteBook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
