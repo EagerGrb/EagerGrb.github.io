2d9e177f111ae079349fe71afdeb3af165cea78d
EagerGrb <282113895@qq.com>
2025-06-15 16:22:08 +0800

    auto build

---
+++ robinNoteBook/docusaurus.config.js
@@ -0,0 +1,15 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'robinNoteBook',
+  tagline: '快乐mil mil 屋',
+  url: 'https://eagergrb.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'EagerGrb',
+  projectName: 'robinNoteBook.github.io',
+}); 
