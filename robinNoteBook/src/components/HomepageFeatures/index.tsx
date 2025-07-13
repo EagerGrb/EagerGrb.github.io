228a1efb079adec060e66c3efc2cdd11ca2eb37f
Robin <282113895@qq.com>
2025-07-13 15:54:22 +0800

    增加咬人小家伙video

--- robinNoteBook/src/components/HomepageFeatures/index.tsx
+++ robinNoteBook/src/components/HomepageFeatures/index.tsx
@@ -1,8 +1,7 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
@@ -42,7 +41,7 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -58,14 +57,14 @@ function Feature({title, Svg, description}: FeatureItem) {
 
 export default function HomepageFeatures(): ReactNode {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      style={{ display: 'block', height: '521px', margin: '0 auto' }}
+    >
+      <source src="/mp4/cat.mp4" type="video/mp4" />
+    </video>
   );
 }
