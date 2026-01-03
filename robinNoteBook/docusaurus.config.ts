39449e04f75046fa4a88226294a4ecf49aa94d72
Robin <282113895@qq.com>
2026-01-03 14:24:59 +0800

    trailingSlash

--- robinNoteBook/docusaurus.config.ts
+++ robinNoteBook/docusaurus.config.ts
@@ -19,7 +19,8 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
-
+  trailingSlash: false,
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EagerGrb', // Usually your GitHub org/user name.
